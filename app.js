67f263419e60aa0880a4ad35961ca5e98ea1f51c
David Finley <dfinley2917@gmail.com>
2018-05-18 13:10:08 -0400

    JWT configured, testing

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth-users', {
-  useMongoClient: true
-});
+mongoose
+	.connect('mongodb://localhost/auth-users', {
+		useMongoClient: true,
+	})
+	.then(() => {
+		console.log('\n=== Connected to MongoDB ===');
+	});
 
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+	console.log(`Server listening on port ${port}`);
 });
